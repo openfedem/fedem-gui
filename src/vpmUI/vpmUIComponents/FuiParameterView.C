e32b61eee34d2859f9222c16319bef009d735351
Knut Morten Okstad <knut.morten.okstad@sap.com>
2023-04-17 07:42:21 +0200

    Qt4 port: Replace class FFuTableView with a simpler and specialized class FuiParameterView for function and control element parameters

---
+++ src/vpmUI/vpmUIComponents/FuiParameterView.C
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2023 SAP SE
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// This file is part of FEDEM - https://openfedem.org
+////////////////////////////////////////////////////////////////////////////////
+
+#include "vpmUI/vpmUIComponents/FuiParameterView.H"
