7ff0e6653a2e4b89f1e1285aca3f8bcd0c976883
Knut Morten Okstad <kmo@openfedem.org>
2025-02-22 08:41:31 +0100

    Avoid crash in the Object browser listview when selecting a header item. Create an item group for Beams to separate them from other Parts. Add callback for sorting when clicking the header. Simplification: The FuiObjectBrowser class does not need the datahandler interface since it only needs a pointer to the selected FmModelMemberBase object, which now is passed by the update() method.

--- src/vpmApp/vpmAppUAMap/FapUAObjectBrowser.C
+++ src/vpmApp/vpmAppUAMap/FapUAObjectBrowser.C
@@ -15,31 +15,19 @@ Fmd_SOURCE_INIT(FAPUAOBJECTBROWSER, FapUAObjectBrowser, FapUAExistenceHandler);
 //----------------------------------------------------------------------------
 
 FapUAObjectBrowser::FapUAObjectBrowser(FuiObjectBrowser* ui)
-  : FapUAExistenceHandler(ui), FapUADataHandler(ui),
+  : FapUAExistenceHandler(ui),
     permSignalConnector(this), modelMemberChangedConnector(this)
 {
   Fmd_CONSTRUCTOR_INIT(FapUAObjectBrowser);
 
-  this->myUI = ui;
-  this->mySelectedFmItem = NULL;
+  myUI = ui;
 
   // Find selection that created this dialog
+  FmModelMemberBase* selectedItem = NULL;
   std::vector<FmModelMemberBase*> objs;
-  FapEventManager::getMMBSelection(objs,mySelectedFmItem);
-  if (!mySelectedFmItem && !objs.empty())
-    mySelectedFmItem = objs.back();
-}
-//----------------------------------------------------------------------------
-
-FFuaUIValues* FapUAObjectBrowser::createValuesObject()
-{
-  return new FuaObjectBrowserValues();
-}
-//----------------------------------------------------------------------------
-
-void FapUAObjectBrowser::getDBValues(FFuaUIValues* values)
-{
-  static_cast<FuaObjectBrowserValues*>(values)->obj = mySelectedFmItem;
+  FapEventManager::getMMBSelection(objs,selectedItem);
+  if (!selectedItem && !objs.empty())
+    myUI->update(objs.back());
 }
 //----------------------------------------------------------------------------
 
@@ -48,17 +36,16 @@ void FapUAObjectBrowser::onPermSelectionChanged(const std::vector<FFaViewItem*>&
                                                 const std::vector<FFaViewItem*>&)
 {
   // Show the last selected model member
-  mySelectedFmItem = NULL;
+  FmModelMemberBase* selectedItem = NULL;
   size_t i = totalSelection.size();
-  while (i > 0 && !mySelectedFmItem)
-    mySelectedFmItem = dynamic_cast<FmModelMemberBase*>(totalSelection[--i]);
+  while (i > 0 && !selectedItem)
+    selectedItem = dynamic_cast<FmModelMemberBase*>(totalSelection[--i]);
 
-  this->updateUI();
+  myUI->update(selectedItem);
 }
 //----------------------------------------------------------------------------
 
 void FapUAObjectBrowser::onModelMemberChanged(FmModelMemberBase* changedObj)
 {
-  if (changedObj == mySelectedFmItem)
-    this->updateUI();
+  myUI->update(changedObj,true);
 }
