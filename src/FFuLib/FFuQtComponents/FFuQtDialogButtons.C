a2e0ceb94de50dcb28cc194648301409d816f61e
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2025-03-30 15:50:09 +0200

    Simplify the FFuDialogButtons class using Qt layout objects to define the widget geometries instead of explicit specification

--- src/FFuLib/FFuQtComponents/FFuQtDialogButtons.C
+++ src/FFuLib/FFuQtComponents/FFuQtDialogButtons.C
@@ -5,35 +5,72 @@
 // This file is part of FEDEM - https://openfedem.org
 ////////////////////////////////////////////////////////////////////////////////
 
-#include <cstring>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 #include "FFuLib/FFuQtComponents/FFuQtPushButton.H"
 #include "FFuLib/FFuQtComponents/FFuQtFrame.H"
 #include "FFuLib/FFuQtComponents/FFuQtDialogButtons.H"
 
 
-FFuQtDialogButtons::FFuQtDialogButtons(QWidget* parent, bool sep,
-				       const char* leftButtonLabel,
-				       const char* midButtonLabel,
-				       const char* rightButtonLabel,
-				       const char* name)
-  : FFuQtMultUIComponent(parent,name)
+FFuQtDialogButtons::FFuQtDialogButtons(QWidget* parent, bool addSeparator,
+                                       const char* name)
+  : QWidget(parent)
 {
-  leftButtonUp  = strcmp(leftButtonLabel ,"") != 0;
-  midButtonUp   = strcmp(midButtonLabel  ,"") != 0;
-  rightButtonUp = strcmp(rightButtonLabel,"") != 0;
+  this->setObjectName(name);
+  this->setWidget(this);
 
-  this->leftButton = new FFuQtPushButton(this);
-  this->midButton = new FFuQtPushButton(this);
-  this->rightButton = new FFuQtPushButton(this);
-
-  if (sep) {
-    FFuQtFrame* mySeparator;
-    this->separator = mySeparator = new FFuQtFrame(this);
-    mySeparator->setFrameStyle(QFrame::HLine | QFrame::Sunken);
+  QWidget* myButtons = new QWidget();
+  QBoxLayout* layout = new QHBoxLayout(myButtons);
+  layout->setContentsMargins(0,0,0,0);
+  FFuQtPushButton* myButton;
+  FFuQtFrame* myEmptyFrame;
+  for (size_t i = 0; i < buttons.size(); i++)
+  {
+    buttons[i] = myButton = new FFuQtPushButton();
+    layout->addWidget(myButton);
+    // Add an empty frame in case the button is hidden (not labelled)
+    spacers[i] = myEmptyFrame = new FFuQtFrame();
+    myEmptyFrame->setFrameStyle(QFrame::NoFrame);
+    myEmptyFrame->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Fixed);
+    layout->addWidget(myEmptyFrame);
   }
-  else
-    this->separator = NULL;
 
-  this->initWidgets(leftButtonLabel,midButtonLabel,rightButtonLabel);
+  layout = new QVBoxLayout(this);
+  if (addSeparator)
+  {
+    QFrame* mySeparator = new QFrame();
+    mySeparator->setFrameStyle(QFrame::HLine | QFrame::Sunken);
+    mySeparator->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Fixed);
+    layout->addWidget(mySeparator);
+  }
+  layout->addWidget(myButtons);
+
+  this->initWidgets();
+}
+
+
+FFuQtDialogButtonsF::FFuQtDialogButtonsF(QWidget* parent) : QFrame(parent)
+{
+  this->setWidget(this);
+
+  QWidget* myButtons = new QWidget();
+  QBoxLayout* layout = new QHBoxLayout(myButtons);
+  layout->setContentsMargins(0,0,0,0);
+  FFuQtPushButton* myButton;
+  for (size_t i = 0; i < buttons.size(); i++)
+  {
+    buttons[i] = myButton = new FFuQtPushButton();
+    layout->addWidget(myButton);
+  }
+
+  layout = new QVBoxLayout(this);
+  layout->setContentsMargins(0,0,0,0);
+  QFrame* mySeparator = new QFrame();
+  mySeparator->setFrameStyle(QFrame::HLine | QFrame::Sunken);
+  mySeparator->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Fixed);
+  layout->addWidget(mySeparator);
+  layout->addWidget(myButtons);
+
+  this->initWidgets();
 }
