bdb485ca54eb6e82a245d59f6e974299c11e38f7
Knut Morten Okstad <kmo@openfedem.org>
2025-05-29 19:46:00 +0200

    Use QLayout managers to place widgets instead of virtual method placeWidgets() and use the FFuQtNotesLabel class for the Notes heading. Add a FFuQtSeparator between the notes and the PushButtons. Let the four push buttons fill the entire width of the dialog box.

--- src/vpmUI/vpmUITopLevels/vpmUIQtTopLevels/FuiQtCreateBeamstringPair.C
+++ src/vpmUI/vpmUITopLevels/vpmUIQtTopLevels/FuiQtCreateBeamstringPair.C
@@ -5,13 +5,17 @@
 // This file is part of FEDEM - https://openfedem.org
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "FuiQtCreateBeamstringPair.H"
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+
 #include "vpmUI/vpmUIComponents/vpmUIQtComponents/FuiQtQueryInputField.H"
 #include "FFuLib/FFuQtComponents/FFuQtLabel.H"
-#include "FFuLib/FFuQtComponents/FFuQtLabelFrame.H"
 #include "FFuLib/FFuQtComponents/FFuQtToggleButton.H"
 #include "FFuLib/FFuQtComponents/FFuQtPushButton.H"
 #include "FFuLib/FFuQtComponents/FFuQtDialogButtons.H"
+
+#include "FuiQtCreateBeamstringPair.H"
 
 
 FuiCreateBeamstringPair* FuiCreateBeamstringPair::create(int xpos, int ypos,
@@ -19,34 +23,85 @@ FuiCreateBeamstringPair* FuiCreateBeamstringPair::create(int xpos, int ypos,
 							 const char* title,
 							 const char* name)
 {
-  return new FuiQtCreateBeamstringPair(0,xpos,ypos,width,height,title,name);
+  return new FuiQtCreateBeamstringPair(xpos,ypos,width,height,title,name);
 }
 
 
-FuiQtCreateBeamstringPair::FuiQtCreateBeamstringPair(QWidget* parent,
-						     int xpos, int ypos,
+FuiQtCreateBeamstringPair::FuiQtCreateBeamstringPair(int xpos, int ypos,
 						     int width, int height,
 						     const char* title,
 						     const char* name)
-  : FFuQtTopLevelShell(parent,xpos,ypos,width,height,title,name,Qt::MSWindowsFixedSizeDialogHint)
+  : FFuQtTopLevelShell(NULL,xpos,ypos,width,height,
+                       title,name,Qt::MSWindowsFixedSizeDialogHint)
 {
-  this->headerImage         = new FFuQtLabel(this);
-  this->beamstringLabel1    = new FFuQtLabel(this);
-  this->beamstringMenu1     = new FuiQtQueryInputField(this);
-  this->beamstringLabel2    = new FFuQtLabel(this);
-  this->beamstringMenu2     = new FuiQtQueryInputField(this);
-  this->stiffnessFunctionLabel = new FFuQtLabel(this);
-  this->stiffnessFunctionMenu  = new FuiQtQueryInputField(this);
-  this->useRadialSpringsToggle = new FFuQtToggleButton(this);
-  this->notesImage          = new FFuQtLabel(this);
-  this->notesLabel          = new FFuQtLabel(this);
-  this->notesSeparator      = new FFuQtLabelFrame(this);
-  this->notesText           = new FFuQtLabel(this);
-  this->buttonSeparator     = new FFuQtLabelFrame(this);
-  this->createButton        = new FFuQtPushButton(this);
-  this->eraseButton         = new FFuQtPushButton(this);
-  this->closeButton         = new FFuQtPushButton(this);
-  this->helpButton          = new FFuQtPushButton(this);
+  headerImage            = new FFuQtLabel();
+  beamstringMenu1        = new FuiQtQueryInputField(NULL);
+  beamstringMenu2        = new FuiQtQueryInputField(NULL);
+  stiffnessFunctionMenu  = new FuiQtQueryInputField(NULL);
+  useRadialSpringsToggle = new FFuQtToggleButton();
+  notesText              = new FFuQtLabel();
+  createButton           = new FFuQtPushButton();
+  eraseButton            = new FFuQtPushButton();
+  closeButton            = new FFuQtPushButton();
+  helpButton             = new FFuQtPushButton();
 
   this->initWidgets();
+
+  QWidget* qMenu1 = new QWidget();
+  QBoxLayout* layout = new QVBoxLayout(qMenu1);
+  layout->setContentsMargins(0,0,0,0);
+  layout->setSpacing(0);
+  layout->addWidget(new QLabel("First beamstring"));
+  layout->addWidget(static_cast<FuiQtQueryInputField*>(beamstringMenu1));
+
+  QWidget* qMenu2 = new QWidget();
+  layout = new QVBoxLayout(qMenu2);
+  layout->setContentsMargins(0,0,0,0);
+  layout->setSpacing(0);
+  layout->addWidget(new QLabel("Second beamstring"));
+  layout->addWidget(static_cast<FuiQtQueryInputField*>(beamstringMenu2));
+
+  QWidget* qMenu3 = new QWidget();
+  layout = new QVBoxLayout(qMenu3);
+  layout->setContentsMargins(0,0,0,0);
+  layout->setSpacing(0);
+  layout->addWidget(new QLabel("Contact stiffness function"));
+  layout->addWidget(static_cast<FuiQtQueryInputField*>(stiffnessFunctionMenu));
+
+  QWidget* qMenus = new QWidget();
+  QGridLayout* l2 = new QGridLayout(qMenus);
+  l2->setContentsMargins(10,5,10,0);
+  l2->setHorizontalSpacing(20);
+  l2->setRowMinimumHeight(0,45);
+  l2->addWidget(qMenu1,0,0);
+  l2->addWidget(qMenu2,0,1);
+  l2->addWidget(qMenu3,0,2);
+  l2->addWidget(dynamic_cast<FFuQtToggleButton*>(useRadialSpringsToggle),1,2);
+
+  QWidget* qNotes = new QWidget();
+  layout = new QVBoxLayout(qNotes);
+  layout->setContentsMargins(10,0,10,0);
+  layout->setSpacing(0);
+  layout->addWidget(new FFuQtNotesLabel());
+  layout->addWidget(static_cast<FFuQtLabel*>(notesText));
+
+  int hspace = headerImage->getWidthHint()/10;
+  QWidget* qButtons = new QWidget();
+  layout = new QHBoxLayout(qButtons);
+  layout->setContentsMargins(10,0,10,0);
+  layout->addWidget(dynamic_cast<FFuQtPushButton*>(createButton));
+  layout->addSpacing(hspace);
+  layout->addWidget(dynamic_cast<FFuQtPushButton*>(eraseButton));
+  layout->addSpacing(hspace);
+  layout->addWidget(dynamic_cast<FFuQtPushButton*>(closeButton));
+  layout->addSpacing(hspace);
+  layout->addWidget(dynamic_cast<FFuQtPushButton*>(helpButton));
+
+  layout = new QVBoxLayout(this);
+  layout->setContentsMargins(0,0,0,10);
+  layout->addWidget(static_cast<FFuQtLabel*>(headerImage));
+  layout->addWidget(qMenus);
+  layout->addWidget(qNotes);
+  layout->addWidget(new FFuQtSeparator());
+  layout->addWidget(qButtons);
 }
