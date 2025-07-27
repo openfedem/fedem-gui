2ce9517d2e76f92294d03e7d32671e88c727662b
Knut Morten Okstad <kmo@openfedem.org>
2025-07-27 09:00:02 +0200

    Simplify the FuiInputSelector widget by inheriting FFuComponentBase instead of FFuMultUIComponent, and let its Qt sub-class inherit FFuQtLabelFrame instead of having it as member. Use QLayout managers to position the child widgets.
    Add container class FuiInputSelectors to be used for multi-argument
    functions. It contains a map of widgets using the number of arguments
    as key and where only one is displayed at any time depending on how
    many arguments the function have. Each of these widgets then contain
    a list of FuiInputSelector instances arranged in a scrollable list.
    This replaces the use of FFuTable which did not work so well when
    using layout managers for the positioning.

--- src/vpmUI/vpmUIComponents/FuiInputSelector.C
+++ src/vpmUI/vpmUIComponents/FuiInputSelector.C
@@ -7,85 +7,38 @@
 
 #include "vpmUI/vpmUIComponents/FuiInputSelector.H"
 #include "vpmUI/vpmUIComponents/FuiQueryInputField.H"
-#include "FFuLib/FFuLabel.H"
-#include "FFuLib/FFuLabelFrame.H"
 #include "FFuLib/FFuOptionMenu.H"
 
 
-FuiInputSelector::FuiInputSelector()
+FuiInputSelector::FuiInputSelector(int iArg)
 {
   IAmSensitive = true;
-  IAmShowingDofList = false;
-  IAmShowingVariableList = false;
+  IAmShowingDofList = IAmShowingVariableList = false;
+  myArgNo = iArg;
 }
 
 
 void FuiInputSelector::initWidgets()
 {
-  myLabelFrame->setLabel("Argument");
+  myObjSelector->setRefSelectedCB(FFaDynCB1M(FuiInputSelector,this,
+                                             onIntChanged,int));
+  myObjSelector->setBehaviour(FuiQueryInputField::REF_NONE);
+  myObjSelector->setButtonMeaning(FuiQueryInputField::SELECT);
 
-  myObjectSelector->setRefSelectedCB(FFaDynCB1M(FuiInputSelector,this,
-						onIntChanged,int));
-  myObjectSelector->setBehaviour(FuiQueryInputField::REF_NONE);
-  myObjectSelector->setButtonMeaning(FuiQueryInputField::SELECT);
+  if (myArgNo >= 0)
+    myObjSelector->setButtonCB(FFaDynCB0M(FuiInputSelector,this,
+                                          onSensorPicked));
 
-  myDofLabel->setLabel("DOF:");
   myDofSelector->setOptionSelectedCB(FFaDynCB1M(FuiInputSelector,this,
                                                 onIntChanged,int));
-  myVariableLabel->setLabel("Var:");
-  myVariableSelector->setOptionSelectedCB(FFaDynCB1M(FuiInputSelector,this,
-                                                     onIntChanged,int));
+  myVarSelector->setOptionSelectedCB(FFaDynCB1M(FuiInputSelector,this,
+                                                onIntChanged,int));
 }
 
-
-void FuiInputSelector::placeWidgets(int width, int height)
-{
-  int relativeOptMHeight = 333;
-
-  int textHeight   = myDofLabel->getHeightHint();
-  int vBorder      = getGridLinePos(height, 15);
-  int hBorder      = 5;
-
-  int o1 = getGridLinePos(height - 8*vBorder - textHeight, relativeOptMHeight);
-  int o2 = myDofSelector->getHeightHint();
-  int optionMenuHeight = o1 < o2 ? o1 : o2;
-  int dofTextWidth = myDofLabel->getWidthHint();
-  int varTextWidth = myVariableLabel->getWidthHint();
-  int maxTextWidth = dofTextWidth > varTextWidth ? dofTextWidth : varTextWidth;
-
-  int v1 = hBorder;
-  int v2 = hBorder + maxTextWidth + hBorder;
-  int v6 = width - hBorder;
-
-  int h1 = 2*vBorder + textHeight;
-  int h2 = h1 + optionMenuHeight;
-  int h3 = h2 + 2*vBorder;
-  int h4 = h3 + optionMenuHeight;
-  int h5 = h4 + 2*vBorder;
-  int h6 = h5 + optionMenuHeight;
-
-  myLabelFrame->      setEdgeGeometry( 0,width,  0, height);
-  myObjectSelector->  setEdgeGeometry(v1,   v6, h1,     h2);
-  myDofLabel->        setEdgeGeometry(v1,   v2, h3,     h4);
-  myDofSelector->     setEdgeGeometry(v2,   v6, h3,     h4);
-  myVariableLabel->   setEdgeGeometry(v1,   v2, h5,     h6);
-  myVariableSelector->setEdgeGeometry(v2,   v6, h5,     h6);
-}
-
-
-void FuiInputSelector::setValuesChangedCB(const FFaDynCB1<FuiInputSelectorValues&>& aDynCB)
-{
-  myValuesChangedCB = aDynCB;
-}
-
-void FuiInputSelector::setSetDBValuesCB(const FFaDynCB1<FuiInputSelectorValues&>& aDynCB)
-{
-  mySetDBValuesCB = aDynCB;
-}
 
 void FuiInputSelector::setPickCB(const FFaDynCB0& aDynCB)
 {
-  myObjectSelector->setButtonCB(aDynCB);
+  myObjSelector->setButtonCB(aDynCB);
 }
 
 
@@ -93,8 +46,8 @@ void FuiInputSelector::setValues(const FuiInputSelectorValues& values)
 {
   // Sensor selector :
 
-  myObjectSelector->setQuery(values.objectSelectorQuery);
-  myObjectSelector->setSelectedRef(values.selectedObject);
+  myObjSelector->setQuery(values.objectSelectorQuery);
+  myObjSelector->setSelectedRef(values.selectedObject);
 
   // DOF Selector :
 
@@ -105,16 +58,20 @@ void FuiInputSelector::setValues(const FuiInputSelectorValues& values)
   // Variable Selector :
 
   IAmShowingVariableList = values.showVariableList;
-  myVariableSelector->setOptions(values.variableList);
-  myVariableSelector->selectOption(values.selectedVariableIdx);
+  myVarSelector->setOptions(values.variableList);
+  myVarSelector->selectOption(values.selectedVariableIdx);
 
-  this->setDBValues();
-  this->updateSensitivity();
+  FuiInputSelectorValues tmp;
+  this->getValues(tmp);
+  mySetDBValuesCB.invoke(tmp);
+
+  this->setSensitivity(IAmSensitive);
 }
+
 
 void FuiInputSelector::getValues(FuiInputSelectorValues& values)
 {
-  values.selectedObject = myObjectSelector->getSelectedRef();
+  values.selectedObject = myObjSelector->getSelectedRef();
 
   if (IAmShowingDofList)
     values.selectedDofIdx = myDofSelector->getSelectedOption();
@@ -122,24 +79,13 @@ void FuiInputSelector::getValues(FuiInputSelectorValues& values)
     values.selectedDofIdx = -1;
 
   if (IAmShowingVariableList)
-    values.selectedVariableIdx = myVariableSelector->getSelectedOption();
+    values.selectedVariableIdx = myVarSelector->getSelectedOption();
   else
     values.selectedVariableIdx = -1;
 }
 
 
-void FuiInputSelector::setLabel(const std::string& label)
-{
-  myLabelFrame->setLabel(label.c_str());
-}
-
-
 void FuiInputSelector::onIntChanged(int)
-{
-  this->onValuesChanged();
-}
-
-void FuiInputSelector::onValuesChanged()
 {
   FuiInputSelectorValues values;
   this->getValues(values);
@@ -147,23 +93,108 @@ void FuiInputSelector::onValuesChanged()
 }
 
 
-void FuiInputSelector::setDBValues()
+void FuiInputSelector::onSensorPicked()
 {
-  FuiInputSelectorValues values;
-  this->getValues(values);
-  mySetDBValuesCB.invoke(values);
+  if (myArgNo >= 0)
+    myPickSensorCB.invoke(myArgNo);
 }
 
 
 void FuiInputSelector::setSensitivity(bool isSensitive)
 {
-  IAmSensitive = isSensitive;
-  this->updateSensitivity();
+  myObjSelector->setSensitivity(IAmSensitive = isSensitive);
+  myDofSelector->setSensitivity(IAmSensitive && IAmShowingDofList);
+  myVarSelector->setSensitivity(IAmSensitive && IAmShowingVariableList);
 }
 
-void FuiInputSelector::updateSensitivity()
+
+void FuiInputSelectors::setCBs(const DynCB1& cb1, const DynCB1& cb2,
+                               const FFaDynCB1<int>& cb3)
 {
-  myObjectSelector->  setSensitivity(IAmSensitive);
-  myDofSelector->     setSensitivity(IAmSensitive && IAmShowingDofList);
-  myVariableSelector->setSensitivity(IAmSensitive && IAmShowingVariableList);
+  myValuesChangedCB = cb1;
+  mySetDBvaluessCB = cb2;
+  mySensorPickedCB = cb3;
+}
+
+
+void FuiInputSelectors::setValues(const Values& vals)
+{
+  std::map<size_t,Sheet*>::iterator it = myLists.find(vals.size());
+  if (it != myLists.end()) it->second->setValues(vals);
+}
+
+
+void FuiInputSelectors::getValues(Values& vals) const
+{
+  std::map<size_t,Sheet*>::const_iterator it = myLists.find(vals.size());
+  if (it != myLists.end()) it->second->getValues(vals);
+}
+
+
+void FuiInputSelectors::setSensitivity(bool isSensitive)
+{
+  for (std::pair<const size_t,Sheet*>& list : myLists)
+    list.second->setSensitivity(isSensitive);
+}
+
+
+void FuiInputSelectors::popUp(size_t numArg)
+{
+  std::map<size_t,Sheet*>::iterator it = myLists.find(numArg);
+  if (it == myLists.end())
+  {
+    it = myLists.insert({ numArg, this->createSheet(numArg) }).first;
+    it->second->initWidgets(myValuesChangedCB,mySetDBvaluessCB,
+                            mySensorPickedCB);
+  }
+
+  for (std::pair<const size_t,Sheet*>& list : myLists)
+    if (list.first == numArg)
+      list.second->popUp();
+    else
+      list.second->popDown();
+}
+
+
+void FuiInputSelectors::popDown()
+{
+  for (std::pair<const size_t,Sheet*>& list : myLists)
+    list.second->popDown();
+}
+
+
+void FuiInputSelectors::Sheet::initWidgets(const DynCB1& valChangedCB,
+                                           const DynCB1& setDBvalsCB,
+                                           const FFaDynCB1<int>& sensorPickedCB)
+{
+  for (size_t i = 0; i < myArgs.size(); i++)
+  {
+    char cArg = myArgs.size() > 4 ? '1'+i : (i < 3 ? 'x'+i : 't');
+    myArgs[i]->setLabel(std::string("Argument ") + cArg);
+    myArgs[i]->setValuesChangedCB(valChangedCB);
+    myArgs[i]->setSetDBValuesCB(setDBvalsCB);
+    myArgs[i]->setPickCB(sensorPickedCB);
+  }
+}
+
+
+void FuiInputSelectors::Sheet::setValues(const Values& vals)
+{
+  for (size_t i = 0; i < vals.size() && i < myArgs.size(); i++)
+    myArgs[i]->setValues(vals[i]);
+}
+
+
+void FuiInputSelectors::Sheet::getValues(Values& vals) const
+{
+  vals.resize(myArgs.size());
+  for (size_t i = 0; i < myArgs.size(); i++)
+    myArgs[i]->getValues(vals[i]);
+}
+
+
+void FuiInputSelectors::Sheet::setSensitivity(bool isSensitive)
+{
+  for (FuiInputSelector* arg : myArgs)
+    arg->setSensitivity(isSensitive);
 }
--- src/vpmUI/vpmUIComponents/vpmUIQtComponents/FuiQtInputSelector.C
+++ src/vpmUI/vpmUIComponents/vpmUIQtComponents/FuiQtInputSelector.C
@@ -5,25 +5,100 @@
 // This file is part of FEDEM - https://openfedem.org
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QResizeEvent>
+#include <QScrollArea>
+#include <QScrollBar>
+#include <QLabel>
+
 #include "FuiQtInputSelector.H"
 #include "vpmUI/vpmUIComponents/vpmUIQtComponents/FuiQtQueryInputField.H"
-#include "FFuLib/FFuQtComponents/FFuQtLabelFrame.H"
-#include "FFuLib/FFuQtComponents/FFuQtLabel.H"
 #include "FFuLib/FFuQtComponents/FFuQtOptionMenu.H"
 
 
-FuiQtInputSelector::FuiQtInputSelector(QWidget* parent,
-				       int xpos, int ypos,
-				       int width,int height,
-				       const char* name)
-  : FFuQtMultUIComponent(parent,xpos,ypos,width,height,name)
+FuiQtInputSelector::FuiQtInputSelector(QWidget* parent, int iArg)
+  : FFuQtLabelFrame(parent), FuiInputSelector(iArg)
 {
-  myLabelFrame = new FFuQtLabelFrame(this);
-  myObjectSelector = new FuiQtQueryInputField(this);
-  myDofLabel = new FFuQtLabel(this);
-  myDofSelector = new FFuQtOptionMenu(this);
-  myVariableLabel = new FFuQtLabel(this);
-  myVariableSelector = new FFuQtOptionMenu(this);
+  if (iArg < 0)
+    this->setObjectName("FuiQtInputSelector");
+  else
+    this->setObjectName(("FuiQtInputSelector" + std::to_string(iArg)).c_str());
+  this->setLabel("Argument");
+
+  myObjSelector = new FuiQtQueryInputField(NULL);
+  myDofSelector = new FFuQtOptionMenu();
+  myVarSelector = new FFuQtOptionMenu();
 
   this->initWidgets();
+
+  QGridLayout* layout = new QGridLayout(this);
+  layout->setContentsMargins(5,0,5,5);
+  layout->setColumnStretch(1,1);
+  layout->addWidget(static_cast<FuiQtQueryInputField*>(myObjSelector), 0,0,1,2);
+  layout->addWidget(new QLabel("DOF:"), 1,0);
+  layout->addWidget(static_cast<FFuQtOptionMenu*>(myDofSelector), 1,1);
+  layout->addWidget(new QLabel("Var:"), 2,0);
+  layout->addWidget(static_cast<FFuQtOptionMenu*>(myVarSelector), 2,1);
+}
+
+
+class FuiQtInputSelectorSheet : public FFuQtWidget,
+                                public FuiInputSelectors::Sheet
+{
+public:
+  FuiQtInputSelectorSheet(QWidget* parent, int numArg, const char* name)
+    : FFuQtWidget(NULL,name)
+  {
+    QScrollArea* mainView = new QScrollArea();
+    mainView->setWidget(myMainWidget = new QWidget(mainView->viewport()));
+    mainView->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+    mainView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    mainView->setFrameStyle(QFrame::WinPanel | QFrame::Sunken);
+
+    myArgs.reserve(numArg);
+    for (int i = 0; i < numArg; i++)
+      myArgs.push_back(new FuiQtInputSelector(myMainWidget,i));
+
+    QLayout* layout = new QVBoxLayout(this);
+    layout->addWidget(mainView);
+
+    static_cast<QBoxLayout*>(parent->layout())->insertWidget(1,this,2);
+  }
+
+  virtual void setSensitivity(bool isSensitive)
+  {
+    this->Sheet::setSensitivity(isSensitive);
+  }
+
+protected:
+  virtual void resizeEvent(QResizeEvent* e)
+  {
+    if (myArgs.empty()) return;
+
+    int height = e->size().height();
+    int width  = e->size().width();
+
+    QWidget* mainView = this->layout()->itemAt(0)->widget();
+    mainView->setGeometry(0,0,width,height);
+
+    QScrollBar* bar = static_cast<QScrollArea*>(mainView)->verticalScrollBar();
+    if (bar->isVisible()) width -= bar->width();
+
+    int ypos = 0;
+    int rowHeight = myArgs.front()->getHeightHint();
+    for (size_t row = 0; row < myArgs.size(); row++, ypos += rowHeight+2)
+      myArgs[row]->setSizeGeometry(2,ypos,width-8,rowHeight);
+    myMainWidget->resize(width,ypos);
+  }
+
+private:
+  QWidget* myMainWidget;
+};
+
+
+FuiInputSelectors::Sheet*
+FuiQtInputSelectors::createSheet(int numArg, const char* name) const
+{
+  return new FuiQtInputSelectorSheet(myParent,numArg,name);
 }
