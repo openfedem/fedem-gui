6784ff3ab1c3a528acd890d4a40b62d59e09d0d4
Knut Morten Okstad <kmo@openfedem.org>
2025-08-17 06:14:05 +0200

    Let FuiGraphDefine inherit FFuComponentBase directly instead of via FFuMultUIComponent. The Qt sub-class then inherits FFuQtWidget with associated layout managers for the positioning instead of using a virtual method placeWidgets() for explicit placement.

--- src/vpmUI/vpmUIComponents/vpmUIQtComponents/FuiQtGraphDefine.C
+++ src/vpmUI/vpmUIComponents/vpmUIQtComponents/FuiQtGraphDefine.C
@@ -5,42 +5,83 @@
 // This file is part of FEDEM - https://openfedem.org
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "vpmUI/vpmUIComponents/vpmUIQtComponents/FuiQtGraphDefine.H"
-#include "FFuLib/FFuQtComponents/FFuQtLabel.H"
-#include "FFuLib/FFuQtComponents/FFuQtIOField.H"
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
+
 #include "FFuLib/FFuQtComponents/FFuQtSpinBox.H"
 #include "FFuLib/FFuQtComponents/FFuQtOptionMenu.H"
 #include "FFuLib/FFuQtComponents/FFuQtToggleButton.H"
 #include "FFuLib/FFuQtComponents/FFuQtLabelField.H"
 
+#include "FuiQtGraphDefine.H"
 
-FuiQtGraphDefine::FuiQtGraphDefine(QWidget* parent,
-				   const char* name)
-  : FFuQtMultUIComponent(parent,name)
+
+FuiQtGraphDefine::FuiQtGraphDefine(QWidget* parent, const char* name)
+  : FFuQtWidget(parent,name)
 {
-  this->FuiGraphDefine::titleField = new FFuQtIOField(this);
-  this->FuiGraphDefine::titleLabel = new FFuQtLabel(this);
-  this->FuiGraphDefine::subtitleField = new FFuQtIOField(this);
-  this->FuiGraphDefine::subtitleLabel = new FFuQtLabel(this);
+  titleField = new FFuQtLabelField();
+  subtitleField = new FFuQtLabelField();
 
-  this->FuiGraphDefine::xAxisField = new FFuQtIOField(this);
-  this->FuiGraphDefine::xAxisLabel = new FFuQtLabel(this);
-  this->FuiGraphDefine::yAxisField = new FFuQtIOField(this);
-  this->FuiGraphDefine::yAxisLabel = new FFuQtLabel(this);
+  xAxisField = new FFuQtLabelField();
+  yAxisField = new FFuQtLabelField();
 
-  for (FFuLabel*& label : this->FuiGraphDefine::fontSizeLabel)
-    label = new FFuQtLabel(this);
-  for (FFuSpinBox*& box : this->FuiGraphDefine::fontSizeBox)
-    box = new FFuQtSpinBox(this);
+  for (FFuSpinBox*& box : fontSizeBox)
+    box = new FFuQtSpinBox();
 
-  this->FuiGraphDefine::gridMenu = new FFuQtOptionMenu(this);
-  this->FuiGraphDefine::gridLabel = new FFuQtLabel(this);
-  this->FuiGraphDefine::legendButton = new FFuQtToggleButton(this);
-  this->FuiGraphDefine::autoscaleButton = new FFuQtToggleButton(this);
+  gridMenu = new FFuQtOptionMenu();
+  legendButton = new FFuQtToggleButton();
+  autoscaleButton = new FFuQtToggleButton();
 
-  this->FuiGraphDefine::tminField = new FFuQtLabelField(this);
-  this->FuiGraphDefine::tmaxField = new FFuQtLabelField(this);
-  this->FuiGraphDefine::timeButton = new FFuQtToggleButton(this);
+  tminField = new FFuQtLabelField();
+  tmaxField = new FFuQtLabelField();
+  timeButton = new FFuQtToggleButton();
 
-  this->FuiGraphDefine::initWidgets();
+  this->initWidgets();
+
+  QWidget* qGridSize = new QWidget();
+  QBoxLayout* aLayout = new QHBoxLayout(qGridSize);
+  aLayout->setContentsMargins(0,0,0,0);
+  aLayout->addWidget(new QLabel("Grid:"));
+  aLayout->addWidget(gridMenu->getQtWidget());
+
+  std::array<QWidget*,3> qFontSize;
+  for (int i = 0; i < 3; i++)
+  {
+    qFontSize[i] = new QWidget();
+    aLayout = new QHBoxLayout(qFontSize[i]);
+    aLayout->setContentsMargins(0,0,0,0);
+    aLayout->addWidget(new QLabel("Font size:"));
+    aLayout->addWidget(fontSizeBox[i]->getQtWidget());
+  }
+
+  QWidget* qUpper = new QWidget();
+  QGridLayout* layout = new QGridLayout(qUpper);
+  layout->setContentsMargins(0,0,0,0);
+  layout->setHorizontalSpacing(20);
+  layout->setVerticalSpacing(2);
+  layout->setColumnStretch(0,1);
+  layout->addWidget(titleField->getQtWidget(), 0,0,1,2);
+  layout->addWidget(subtitleField->getQtWidget(), 1,0,1,2);
+  layout->addWidget(xAxisField->getQtWidget(), 2,0);
+  layout->addWidget(yAxisField->getQtWidget(), 3,0);
+  layout->addWidget(qFontSize[0], 0,2);
+  layout->addWidget(qGridSize, 1,2);
+  layout->addWidget(qFontSize[1], 2,1);
+  layout->addWidget(qFontSize[2], 3,1);
+  layout->addWidget(legendButton->getQtWidget(), 2,2);
+  layout->addWidget(autoscaleButton->getQtWidget(), 3,2);
+
+  QWidget* qLower = new QWidget();
+  aLayout = new QHBoxLayout(qLower);
+  aLayout->setContentsMargins(0,0,0,0);
+  aLayout->setSpacing(20);
+  aLayout->addWidget(tminField->getQtWidget(),1);
+  aLayout->addWidget(tmaxField->getQtWidget(),1);
+  aLayout->addWidget(timeButton->getQtWidget());
+
+  aLayout = new QVBoxLayout(this);
+  aLayout->addWidget(qUpper);
+  aLayout->addWidget(qLower);
 }
