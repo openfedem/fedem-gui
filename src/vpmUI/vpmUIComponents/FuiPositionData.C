openfedem/fedem-gui

commit d605cf42e7c3f1cc0203a0e596abc89f151a73c6

Move the two frame members up from the Qt sub-class and make the methods adjusting them non-virtual. Remove unnecessary override of methods get[Height|Width]Hint(). Merge method updateSensitivity() with setSensitivity() and remove set[Rot|Pos]Sensitivity() and isUsing6RotParams() methods. Use std::array instead of std::vector for the labels and fields. Make the callback methods private. Finally, some cosmetic layout adjustments.

src/vpmUI/vpmUIComponents/FuiPositionData.C
@@ -13,10 +13,11 @@
 #include "vpmUI/vpmUITopLevels/FuiProperties.H"
 #include "vpmUI/Fui.H"
 #include "vpmUI/FuiModes.H"
+
+#include "FFuLib/FFuLabelFrame.H"
+#include "FFuLib/FFuLabel.H"
 #include "FFuLib/FFuIOField.H"
 #include "FFuLib/FFuOptionMenu.H"
-#include "FFuLib/FFuLabel.H"
-#include "FFuLib/FFuLabelFrame.H"
 #include "FFuLib/FFuToggleButton.H"
 
 #include "vpmDB/FmDB.H"
@@ -70,34 +71,32 @@ FuiPositionData::FuiPositionData() : signalConnector(this)
 
 void FuiPositionData::initWidgets()
 {
-  myPosRefCSField->popUp();
   myPosRefCSField->setBehaviour(FuiQueryInputField::REF_NONE);
   myPosRefCSField->setRefSelectedCB(FFaDynCB1M(FuiPositionData,this,
-                                               onPosRefChanged, FmModelMemberBase*));
-  myPosRefCSField->setButtonCB(FFaDynCB0M(FuiPositionData,this, onPosRefButtonPressed));
+                                               onPosRefChanged,FmModelMemberBase*));
+  myPosRefCSField->setButtonCB(FFaDynCB0M(FuiPositionData,this,
+                                          onPosRefButtonPressed));
   myPosRefCSField->setButtonMeaning(FuiQueryInputField::SELECT);
   myPosRefCSField->setTextForNoRefSelected("Global");
 
-  myRotRefCSField->popUp();
   myRotRefCSField->setBehaviour(FuiQueryInputField::REF_NONE);
   myRotRefCSField->setRefSelectedCB(FFaDynCB1M(FuiPositionData,this,
-                                               onRotRefChanged, FmModelMemberBase*));
-  myRotRefCSField->setButtonCB(FFaDynCB0M(FuiPositionData,this, onRotRefButtonPressed));
+                                               onRotRefChanged,FmModelMemberBase*));
+  myRotRefCSField->setButtonCB(FFaDynCB0M(FuiPositionData,this,
+                                          onRotRefButtonPressed));
   myRotRefCSField->setButtonMeaning(FuiQueryInputField::SELECT);
   myRotRefCSField->setTextForNoRefSelected("Global");
 
   size_t i;
   for (i = 0; i < PosTypeMapping::map().size(); i++)
     myPosViewTypeMenu->addOption(PosTypeMapping::map()[i].second, i);
-
-  myPosViewTypeMenu->setOptionSelectedCB(FFaDynCB1M(FuiPositionData,this,onPosTypeChanged, int));
-  myPosViewTypeMenu->popUp();
+  myPosViewTypeMenu->setOptionSelectedCB(FFaDynCB1M(FuiPositionData,this,
+                                                    onPosTypeChanged,int));
 
   for (i = 0; i < RotTypeMapping::map().size(); i++)
     myRotViewTypeMenu->addOption(RotTypeMapping::map()[i].second, i);
-
-  myRotViewTypeMenu->setOptionSelectedCB(FFaDynCB1M(FuiPositionData,this,onRotTypeChanged, int));
-  myRotViewTypeMenu->popUp();
+  myRotViewTypeMenu->setOptionSelectedCB(FFaDynCB1M(FuiPositionData,this,
+                                                    onRotTypeChanged,int));
 
   for (FFuIOField* fld : myFields) {
     fld->popUp();
@@ -105,34 +104,38 @@ void FuiPositionData::initWidgets()
     fld->setAcceptedCB(FFaDynCB1M(FuiPositionData,this,onFieldAccepted,double));
   }
 
-  for (FFuLabel* lab : myLabels) {
-    lab->popUp();
-    lab->toFront();
-  }
-
-  myMasterFollowToggle->setToggleCB(FFaDynCB1M(FuiPositionData,this,onTriadsFollowToggled,bool));
-  mySlaveFollowToggle->setToggleCB(FFaDynCB1M(FuiPositionData,this,onTriadsFollowToggled,bool));
+  myMasterFollowToggle->setToggleCB(FFaDynCB1M(FuiPositionData,this,
+                                               onTriadsFollowToggled,bool));
+  mySlaveFollowToggle->setToggleCB(FFaDynCB1M(FuiPositionData,this,
+                                              onTriadsFollowToggled,bool));
 }
 
 
-bool FuiPositionData::isUsing6RotParams()
+void FuiPositionData::setPosLabel(const char* text)
 {
-  if (!myEditedObj) return false;
+  myTranslationFrame->setLabel(text);
+}
 
-  if (IAmEditingLinkCG)
-    return (static_cast<FmPart*>(myEditedObj)->getLocationCG().getNumFields() == 9);
-  else if (myEditedObj->isOfType(FmIsPositionedBase::getClassTypeID()))
-    return (static_cast<FmIsPositionedBase*>(myEditedObj)->getLocation().getNumFields() == 9);
-  else if (myEditedObj->isOfType(FmAssemblyBase::getClassTypeID()))
-    return (static_cast<FmAssemblyBase*>(myEditedObj)->getLocation().getNumFields() == 9);
-
-  return false;
+void FuiPositionData::setRotLabel(const char* text)
+{
+  myRotationFrame->setLabel(text);
 }
 
 
-void FuiPositionData::editLinkCG(bool pleaseDo)
+void FuiPositionData::popUpPosUI(bool onOff)
 {
-  IAmEditingLinkCG = pleaseDo;
+  if (onOff)
+    myTranslationFrame->popUp();
+  else
+    myTranslationFrame->popDown();
+}
+
+void FuiPositionData::popUpRotUI(bool onOff)
+{
+  if (onOff)
+    myRotationFrame->popUp();
+  else
+    myRotationFrame->popDown();
 }
 
 
@@ -155,10 +158,19 @@ void FuiPositionData::updateUI()
   if (!myEditedObj) return;
 
   FFa3DLocation loc;
-  if (IAmEditingLinkCG)
-    loc = static_cast<FmPart*>(myEditedObj)->getLocationCG();
-  else if (myEditedObj->isOfType(FmIsPositionedBase::getClassTypeID()))
-    loc = static_cast<FmIsPositionedBase*>(myEditedObj)->getLocation();
+  FmIsPositionedBase* posRef = NULL;
+  FmIsPositionedBase* rotRef = NULL;
+
+  if (myEditedObj->isOfType(FmIsPositionedBase::getClassTypeID()))
+  {
+    FmIsPositionedBase* obj = static_cast<FmIsPositionedBase*>(myEditedObj);
+    posRef = obj->getPosRef();
+    rotRef = obj->getRotRef();
+    if (IAmEditingLinkCG)
+      loc = static_cast<FmPart*>(myEditedObj)->getLocationCG();
+    else
+      loc = obj->getLocation();
+  }
   else if (myEditedObj->isOfType(FmAssemblyBase::getClassTypeID()))
     loc = static_cast<FmAssemblyBase*>(myEditedObj)->getLocation();
   else
@@ -177,21 +189,9 @@ void FuiPositionData::updateUI()
     for (j = 0; j < 3; j++)
       myFields[3*i+j]->setValue(loc[i][j]);
 
-  FmIsPositionedBase* posRef = NULL;
-  FmIsPositionedBase* rotRef = NULL;
-
-  if (IAmEditingLinkCG) {
-    posRef = static_cast<FmPart*>(myEditedObj)->getCGPosRef();
-    rotRef = static_cast<FmPart*>(myEditedObj)->getCGRotRef();
-  }
-  else if (myEditedObj->isOfType(FmIsPositionedBase::getClassTypeID())) {
-    posRef = static_cast<FmIsPositionedBase*>(myEditedObj)->getPosRef();
-    rotRef = static_cast<FmIsPositionedBase*>(myEditedObj)->getRotRef();
-  }
-
   static FapUAQuery query1;
   if (myEditedObj->isOfType(FmIsPositionedBase::getClassTypeID()))
-    query1.typesToFind.insert(std::make_pair(FmIsPositionedBase::getClassTypeID(),true));
+    query1.typesToFind.emplace(FmIsPositionedBase::getClassTypeID(),true);
   myPosRefCSField->setQuery(&query1);
   myRotRefCSField->setQuery(&query1);
 
@@ -210,9 +210,9 @@ void FuiPositionData::updateUI()
       break;
     }
 
-  this->updateSensitivity();
+  this->setSensitivity(IAmEditable);
 
-  if (this->isUsing6RotParams())
+  if (loc.getNumFields() == 9)
     for (i = 6; i < 9; i++) {
       myFields[i]->popUp();
       myLabels[i]->popUp();
@@ -301,47 +301,30 @@ void FuiPositionData::updateUI()
 }
 
 
-void FuiPositionData::setPosSensitivity(bool isSensitive)
+void FuiPositionData::setSensitivity(bool onOff)
 {
-  myFields[0]->setSensitivity(isSensitive);
-  myFields[1]->setSensitivity(isSensitive);
-  myFields[2]->setSensitivity(isSensitive);
-}
+  IAmEditable = onOff;
+  bool canTra = onOff;
+  bool canRot = onOff;
 
+  if (!myEditedObj)
+    canTra = canRot = false;
+  else if (!IAmEditingLinkCG)
+  {
+    if (myEditedObj->isOfType(FmIsPositionedBase::getClassTypeID()))
+    {
+      canTra = static_cast<FmIsPositionedBase*>(myEditedObj)->isTranslatable();
+      canRot = static_cast<FmIsPositionedBase*>(myEditedObj)->isRotatable();
+    }
+    else if (myEditedObj->isOfType(FmAssemblyBase::getClassTypeID()))
+      canTra = canRot = static_cast<FmAssemblyBase*>(myEditedObj)->isMovable();
+  }
 
-void FuiPositionData::setRotSensitivity(bool isSensitive)
-{
-  myFields[3]->setSensitivity(isSensitive);
-  myFields[4]->setSensitivity(isSensitive);
-  myFields[5]->setSensitivity(isSensitive);
-  myFields[6]->setSensitivity(isSensitive);
-  myFields[7]->setSensitivity(isSensitive);
-  myFields[8]->setSensitivity(isSensitive);
-}
+  for (int i = 0; i < 9; i++)
+    myFields[i]->setSensitivity(i < 3 ? canTra : canRot);
 
-
-void FuiPositionData::updateSensitivity()
-{
   myMasterFollowToggle->setSensitivity(IAmEditable);
   mySlaveFollowToggle->setSensitivity(IAmEditable);
-
-  if (!IAmEditable || !myEditedObj) {
-    this->setRotSensitivity(false);
-    this->setPosSensitivity(false);
-  }
-  else if (IAmEditingLinkCG) {
-    this->setRotSensitivity(true);
-    this->setPosSensitivity(true);
-  }
-  else if (myEditedObj->isOfType(FmIsPositionedBase::getClassTypeID())) {
-    this->setRotSensitivity(static_cast<FmIsPositionedBase*>(myEditedObj)->isRotatable());
-    this->setPosSensitivity(static_cast<FmIsPositionedBase*>(myEditedObj)->isTranslatable());
-  }
-  else if (myEditedObj->isOfType(FmAssemblyBase::getClassTypeID())) {
-    bool canMove = static_cast<FmAssemblyBase*>(myEditedObj)->isMovable();
-    this->setRotSensitivity(canMove);
-    this->setPosSensitivity(canMove);
-  }
 }
 
 

src/vpmUI/vpmUIComponents/FuiPositionData.H
@@ -10,51 +10,51 @@
 
 #include "FFuLib/FFuBase/FFuMultUIComponent.H"
 #include "vpmApp/FapEventManager.H"
-#include <string>
+#include <array>
 #include <vector>
 
-class FuiQueryInputField;
-class FFuOptionMenu;
-class FFuIOField;
-class FFuLabel;
-class FFaViewItem;
-class FmModelMemberBase;
 class FFuLabelFrame;
+class FFuLabel;
+class FFuIOField;
+class FFuOptionMenu;
 class FFuToggleButton;
+class FFaViewItem;
+class FuiQueryInputField;
+class FmModelMemberBase;
 
 
 class FuiPositionData : public virtual FFuMultUIComponent
 {
 public:
   FuiPositionData();
-  virtual ~FuiPositionData() {}
 
   void setEditedObj(FmModelMemberBase* obj);
   FmModelMemberBase* getEditedObj() { return myEditedObj; }
+  void editLinkCG(bool onOff) { IAmEditingLinkCG = onOff; }
 
   static void cancelActiveUI();
 
   void updateUI();
-  void editLinkCG(bool yep);
+  void setSensitivity(bool onOff);
 
-  void setSensitivity(bool onOff) { IAmEditable = onOff; this->updateSensitivity(); }
+  void setPosLabel(const char* text);
+  void setRotLabel(const char* text);
 
-  virtual void setPosLabel(const std::string& text) = 0;
-  virtual void setRotLabel(const std::string& text) = 0;
-
-  virtual void popUpRotUI(bool onOff) = 0;
-  virtual void popUpPosUI(bool onOff) = 0;
+  void popUpRotUI(bool onOff);
+  void popUpPosUI(bool onOff);
 
   void finishRefCSSelection();
 
   FuiQueryInputField* myPosRefCSField;
   FuiQueryInputField* myRotRefCSField;
 
+  FFuLabelFrame* myTranslationFrame;
+  FFuLabelFrame* myRotationFrame;
   FFuOptionMenu* myPosViewTypeMenu;
   FFuOptionMenu* myRotViewTypeMenu;
 
-  std::vector<FFuIOField*> myFields;
-  std::vector<FFuLabel*>   myLabels;
+  std::array<FFuLabel*,9>   myLabels;
+  std::array<FFuIOField*,9> myFields;
 
   FFuLabelFrame*   myTriadPosFollowFrame;
   FFuToggleButton* myMasterFollowToggle;
@@ -62,22 +62,8 @@ public:
 
 protected:
   void initWidgets();
-  void setRotSensitivity(bool isSensitive);
-  void setPosSensitivity(bool isSensitive);
-
-  bool isUsing6RotParams();
 
   FmModelMemberBase* myEditedObj;
-
-  void onPosTypeChanged(int option);
-  void onRotTypeChanged(int option);
-  void onFieldAccepted(double d);
-  void onPosRefChanged(FmModelMemberBase* obj);
-  void onRotRefChanged(FmModelMemberBase* obj);
-  void onRotRefButtonPressed();
-  void onPosRefButtonPressed();
-  void onTriadsFollowToggled(bool val);
-  void updateSensitivity();
 
   virtual void onPoppedUp();
   virtual void onPoppedDown();
@@ -90,9 +76,18 @@ private:
 
   void prepareRefCSSelection();
 
+  void onPosTypeChanged(int option);
+  void onRotTypeChanged(int option);
+  void onFieldAccepted(double d);
+  void onPosRefChanged(FmModelMemberBase* obj);
+  void onRotRefChanged(FmModelMemberBase* obj);
+  void onRotRefButtonPressed();
+  void onPosRefButtonPressed();
+  void onTriadsFollowToggled(bool val);
+
   void onPermSelectionChanged(const std::vector<FFaViewItem*>& totalSelection,
-                              const std::vector<FFaViewItem*>& selectedSinceLast,
-                              const std::vector<FFaViewItem*>& unselectedSinceLast);
+                              const std::vector<FFaViewItem*>&,
+                              const std::vector<FFaViewItem*>&);
 
   FapPermSelChangedReceiver<FuiPositionData> signalConnector;
   friend class FapPermSelChangedReceiver<FuiPositionData>;

src/vpmUI/vpmUIComponents/vpmUIQtComponents/FuiQtPositionData.C
@@ -7,14 +7,12 @@
 
 #include <QGridLayout>
 #include <QVBoxLayout>
-#include <QGroupBox>
 #include <QLabel>
-#include <array>
 
 #include "FFuLib/FFuQtComponents/FFuQtLabelFrame.H"
-#include "FFuLib/FFuQtComponents/FFuQtOptionMenu.H"
-#include "FFuLib/FFuQtComponents/FFuQtIOField.H"
 #include "FFuLib/FFuQtComponents/FFuQtLabel.H"
+#include "FFuLib/FFuQtComponents/FFuQtIOField.H"
+#include "FFuLib/FFuQtComponents/FFuQtOptionMenu.H"
 #include "FFuLib/FFuQtComponents/FFuQtToggleButton.H"
 #include "vpmUI/vpmUIComponents/vpmUIQtComponents/FuiQtQueryInputField.H"
 #include "vpmUI/vpmUIComponents/vpmUIQtComponents/FuiQtPositionData.H"
@@ -24,174 +22,106 @@ FuiQtPositionData::FuiQtPositionData(QWidget* parent, int xpos, int ypos,
                                      int width, int height, const char* name)
   : FFuQtMultUIComponent(parent,xpos,ypos,width,height,name)
 {
-  FuiQtQueryInputField* myQtRotRefCSField;
-  FuiQtQueryInputField* myQtPosRefCSField;
+  FFuQtLabel*   qLabel;
+  FFuQtIOField* qField;
 
-  FFuQtLabel*  qLabel;
   QGridLayout* gLayout;
   QVBoxLayout* vLayout;
 
-  std::array<FFuQtIOField*,9> lineEdit;
-  int i;
-
-  this->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-  QGridLayout* myLayout = new QGridLayout(this);
-  myLayout->setContentsMargins(2,2,2,2);
-  myLayout->setSpacing(2);
-
   // The translation frame
-  myTranslationFrame = new QGroupBox(this);
-  myTranslationFrame->setTitle("Position");
-  myTranslationFrame->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-  vLayout = new QVBoxLayout();
-  vLayout->setContentsMargins(3,3,3,3);
-  vLayout->setSpacing(0);
-  vLayout->setAlignment(Qt::AlignTop);
+  FFuQtLabelFrame* translationFrame = new FFuQtLabelFrame();
+  translationFrame->setTitle("Position");
+
+  vLayout = new QVBoxLayout(translationFrame);
+  vLayout->setContentsMargins(5,3,5,5);
+  vLayout->setSpacing(2);
+
+  FFuQtOptionMenu* posViewTypeMenu = new FFuQtOptionMenu();
+  FuiQtQueryInputField* posRefCSField = new FuiQtQueryInputField(NULL);
+  posRefCSField->setMinimumHeight(posViewTypeMenu->getHeightHint());
 
   gLayout = new QGridLayout();
-  gLayout->addWidget(new QLabel("Reference CS", myTranslationFrame), 0, 0);
-  gLayout->addWidget(new QLabel("Coordinate type", myTranslationFrame), 0, 1);
-
-  myQtPosRefCSField = new FuiQtQueryInputField(myTranslationFrame);
-  myQtPosRefCSField->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-  gLayout->addWidget(myQtPosRefCSField, 1, 0);
-
-  FFuQtOptionMenu* myQtPosViewTypeMenu = new FFuQtOptionMenu(myTranslationFrame);
-  myQtPosViewTypeMenu->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-  gLayout->addWidget(myQtPosViewTypeMenu, 1, 1);
-
+  gLayout->setHorizontalSpacing(2);
+  gLayout->addWidget(new QLabel("Reference CS"), 0, 0);
+  gLayout->addWidget(new QLabel("Coordinate type"), 0, 1);
+  gLayout->addWidget(posRefCSField, 1, 0);
+  gLayout->addWidget(posViewTypeMenu, 1, 1);
   vLayout->addLayout(gLayout);
 
   gLayout = new QGridLayout();
-  gLayout->setSpacing(2);
-
-  for (i = 0; i < 3; i++) {
-    lineEdit[i] = new FFuQtIOField(myTranslationFrame);
-    gLayout->addWidget(lineEdit[i], 1, i);
-    myLabels.push_back(qLabel = new FFuQtLabel(myTranslationFrame));
+  gLayout->setHorizontalSpacing(2);
+  for (int i = 0; i < 3; i++) {
+    myLabels[i] = qLabel = new FFuQtLabel();
+    myFields[i] = qField = new FFuQtIOField();
     gLayout->addWidget(qLabel, 0, i);
+    gLayout->addWidget(qField, 1, i);
   }
   vLayout->addLayout(gLayout);
-  myTranslationFrame->setLayout(vLayout);
-  myLayout->addWidget(myTranslationFrame, 0, 0, 2, 1, Qt::AlignTop);
 
   // The rotation frame
-  myRotationFrame = new QGroupBox(this);
-  myRotationFrame->setTitle("Orientation");
-  myRotationFrame->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-  vLayout = new QVBoxLayout();
-  vLayout->setContentsMargins(3,3,3,3);
-  vLayout->setSpacing(0);
-  vLayout->setAlignment(Qt::AlignTop);
+  FFuQtLabelFrame* rotationFrame = new FFuQtLabelFrame();
+  rotationFrame->setTitle("Orientation");
+
+  vLayout = new QVBoxLayout(rotationFrame);
+  vLayout->setContentsMargins(5,3,5,5);
+  vLayout->setSpacing(2);
+
+  FFuQtOptionMenu* rotViewTypeMenu = new FFuQtOptionMenu();
+  FuiQtQueryInputField* rotRefCSField = new FuiQtQueryInputField(NULL);
+  rotRefCSField->setMinimumHeight(rotViewTypeMenu->getHeightHint());
 
   gLayout = new QGridLayout();
-  gLayout->addWidget(new QLabel("Reference CS", myRotationFrame), 0, 0);
-  gLayout->addWidget(new QLabel("Coordinate type", myRotationFrame), 0, 1);
-
-  myQtRotRefCSField = new FuiQtQueryInputField(myRotationFrame);
-  myQtRotRefCSField->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-  gLayout->addWidget(myQtRotRefCSField, 1, 0);
-
-  FFuQtOptionMenu* myQtRotViewTypeMenu = new FFuQtOptionMenu(myRotationFrame);
-  myQtRotViewTypeMenu->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-  gLayout->addWidget(myQtRotViewTypeMenu, 1, 1);
-
+  gLayout->setHorizontalSpacing(2);
+  gLayout->addWidget(new QLabel("Reference CS"), 0, 0);
+  gLayout->addWidget(new QLabel("Coordinate type"), 0, 1);
+  gLayout->addWidget(rotRefCSField, 1, 0);
+  gLayout->addWidget(rotViewTypeMenu, 1, 1);
   vLayout->addLayout(gLayout);
 
   gLayout = new QGridLayout();
-  gLayout->setSpacing(2);
-
-  for (i = 3; i < 9; i++)
-    lineEdit[i] = new FFuQtIOField(myRotationFrame);
-  for (i = 3; i < 6; i++)
-    gLayout->addWidget(lineEdit[i], 1, i-3);
-  for (i = 6; i < 9; i++)
-    gLayout->addWidget(lineEdit[i], 3, i-6);
-  for (i = 3; i < 9; i++) {
-    myLabels.push_back(qLabel = new FFuQtLabel(myRotationFrame));
-    if (i < 6)
-      gLayout->addWidget(qLabel, 0, i-3);
-    else
-      gLayout->addWidget(qLabel, 2, i-6);
+  gLayout->setHorizontalSpacing(2);
+  for (int i = 3; i < 9; i++) {
+    myLabels[i] = qLabel = new FFuQtLabel();
+    myFields[i] = qField = new FFuQtIOField();
+    gLayout->addWidget(qLabel, i < 6 ? 0 : 2, i%3);
+    gLayout->addWidget(qField, i < 6 ? 1 : 3, i%3);
   }
   vLayout->addLayout(gLayout);
-  myRotationFrame->setLayout(vLayout);
-  myLayout->addWidget(myRotationFrame, 0, 1, Qt::AlignTop);
 
-  // Position follow toggle group:
-  FFuQtLabelFrame* followFrame = new FFuQtLabelFrame(this);
+  // Position follow toggle group
+  FFuQtLabelFrame* followFrame = new FFuQtLabelFrame();
   followFrame->setLabel("Relative positioning of Triads");
-  followFrame->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-  vLayout = new QVBoxLayout();
-  vLayout->setContentsMargins(3,3,3,3);
-  vLayout->setSpacing(2);
-  vLayout->setAlignment(Qt::AlignTop);
-
   FFuQtToggleButton* masterToggle = new FFuQtToggleButton();
   masterToggle->setText("Master triad follows joint");
-  vLayout->addWidget(masterToggle);
-
   FFuQtToggleButton* slaveToggle = new FFuQtToggleButton();
   slaveToggle->setText("Slave triad follows joint");
-  vLayout->addWidget(slaveToggle);
 
-  followFrame->setLayout(vLayout);
-  myLayout->addWidget(followFrame, 1, 1, Qt::AlignTop);
+  myTranslationFrame = translationFrame;
+  myRotationFrame = rotationFrame;
 
-  myQtPosRefCSField->setMinimumHeight(myQtRotViewTypeMenu->getHeightHint());
-  myQtRotRefCSField->setMinimumHeight(myQtRotViewTypeMenu->getHeightHint());
+  myPosRefCSField = posRefCSField;
+  myRotRefCSField = rotRefCSField;
 
-  myPosRefCSField = myQtPosRefCSField;
-  myRotRefCSField = myQtRotRefCSField;
-
-  myPosViewTypeMenu = myQtPosViewTypeMenu;
-  myRotViewTypeMenu = myQtRotViewTypeMenu;
-
-  for (FFuQtIOField* field : lineEdit)
-    myFields.push_back(field);
+  myPosViewTypeMenu = posViewTypeMenu;
+  myRotViewTypeMenu = rotViewTypeMenu;
 
   myTriadPosFollowFrame = followFrame;
   myMasterFollowToggle = masterToggle;
   mySlaveFollowToggle = slaveToggle;
 
   this->initWidgets();
-}
 
+  // Layout for the position follow frame
+  vLayout = new QVBoxLayout(followFrame);
+  vLayout->setContentsMargins(10,3,10,3);
+  vLayout->setSpacing(2);
+  vLayout->addWidget(masterToggle);
+  vLayout->addWidget(slaveToggle);
 
-int FuiQtPositionData::getHeightHint()
-{
-  return this->sizeHint().height();
-}
-
-int FuiQtPositionData::getWidthHint()
-{
-  return this->sizeHint().width();
-}
-
-
-void FuiQtPositionData::setPosLabel(const std::string& text)
-{
-  myTranslationFrame->setTitle(text.c_str());
-}
-
-void FuiQtPositionData::setRotLabel(const std::string& text)
-{
-  myRotationFrame->setTitle(text.c_str());
-}
-
-
-void FuiQtPositionData::popUpRotUI(bool onOff)
-{
-  if (onOff)
-    myRotationFrame->show();
-  else
-    myRotationFrame->hide();
-}
-
-void FuiQtPositionData::popUpPosUI(bool onOff)
-{
-  if (onOff)
-    myTranslationFrame->show();
-  else
-    myTranslationFrame->hide();
+  // The main layout
+  gLayout = new QGridLayout(this);
+  gLayout->setContentsMargins(2,2,2,2);
+  gLayout->addWidget(translationFrame, 0, 0, 2, 1, Qt::AlignTop);
+  gLayout->addWidget(rotationFrame, 0, 1, Qt::AlignTop);
+  gLayout->addWidget(followFrame, 1, 1, Qt::AlignTop);
 }

src/vpmUI/vpmUIComponents/vpmUIQtComponents/FuiQtPositionData.H
@@ -8,12 +8,8 @@
 #ifndef FUI_QT_POSITION_DATA_H
 #define FUI_QT_POSITION_DATA_H
 
-#include <string>
-
 #include "FFuLib/FFuQtBaseClasses/FFuQtMultUIComponent.H"
 #include "vpmUI/vpmUIComponents/FuiPositionData.H"
-
-class QGroupBox;
 
 
 class FuiQtPositionData : public virtual FFuQtMultUIComponent, 
@@ -26,19 +22,10 @@ public:
 		    const char* name="PositionData");
   virtual ~FuiQtPositionData() {}
 
-  virtual int getHeightHint();
-  virtual int getWidthHint();
-
-  virtual void setPosLabel(const std::string& text); 
-  virtual void setRotLabel(const std::string& text);
-
-  virtual void popUpRotUI(bool onOff);
-  virtual void popUpPosUI(bool onOff);
-
-  QGroupBox* myRotationFrame;
-  QGroupBox* myTranslationFrame;
-
-  virtual void setSensitivity(bool onOff) { this->FuiPositionData::setSensitivity(onOff); }
+  virtual void setSensitivity(bool onOff)
+  {
+    this->FuiPositionData::setSensitivity(onOff);
+  }
 };
 
 #endif

commit 16c5a6eda802c402150340723b93d60d9cd420e2

Fix issue #71: Set position and rotation reference correctly when editing the Part CoG. This bug was introduced in commit d605cf42e7c3f1cc0203a0e596abc89f151a73c6.
Also clear the position/rotation reference menus when editing
sub-assembly properties, then only "Global" is relevant.

src/vpmUI/vpmUIComponents/FuiPositionData.C
@@ -163,16 +163,21 @@ void FuiPositionData::updateUI()
   FFa3DLocation loc;
   FmIsPositionedBase* posRef = NULL;
   FmIsPositionedBase* rotRef = NULL;
+  FapUAQuery*         refQue = NULL;
 
-  if (myEditedObj->isOfType(FmIsPositionedBase::getClassTypeID()))
+  if (IAmEditingLinkCG)
+  {
+    FmPart* part = static_cast<FmPart*>(myEditedObj);
+    loc    = part->getLocationCG();
+    posRef = part->getCGPosRef();
+    rotRef = part->getCGRotRef();
+  }
+  else if (myEditedObj->isOfType(FmIsPositionedBase::getClassTypeID()))
   {
     FmIsPositionedBase* obj = static_cast<FmIsPositionedBase*>(myEditedObj);
+    loc    = obj->getLocation();
     posRef = obj->getPosRef();
     rotRef = obj->getRotRef();
-    if (IAmEditingLinkCG)
-      loc = static_cast<FmPart*>(myEditedObj)->getLocationCG();
-    else
-      loc = obj->getLocation();
   }
   else if (myEditedObj->isOfType(FmAssemblyBase::getClassTypeID()))
     loc = static_cast<FmAssemblyBase*>(myEditedObj)->getLocation();
@@ -180,8 +185,9 @@ void FuiPositionData::updateUI()
     return;
 
 #ifdef FUI_DEBUG
-  std::cout <<"FuiPositionData::updateUI: Location for "
-            << myEditedObj->getIdString() << loc << std::endl;
+  std::cout <<"FuiPositionData::updateUI: ";
+  if (IAmEditingLinkCG) std::cout <<"CoG ";
+  std::cout <<"Location for "<< myEditedObj->getIdString() << loc << std::endl;
 #endif
 
   PosType posType = loc.getPosType();
@@ -192,13 +198,16 @@ void FuiPositionData::updateUI()
     for (j = 0; j < 3; j++)
       myFields[3*i+j]->setValue(loc[i][j]);
 
-  static FapUAQuery query1;
   if (myEditedObj->isOfType(FmIsPositionedBase::getClassTypeID()))
+  {
+    static FapUAQuery query1;
     query1.typesToFind.emplace(FmIsPositionedBase::getClassTypeID(),true);
-  myPosRefCSField->setQuery(&query1);
-  myRotRefCSField->setQuery(&query1);
+    refQue = &query1;
+  }
 
+  myPosRefCSField->setQuery(refQue);
   myPosRefCSField->setSelectedRef(posRef);
+  myRotRefCSField->setQuery(refQue);
   myRotRefCSField->setSelectedRef(rotRef);
 
   for (i = 0; i < PosTypeMapping::map().size(); i++)
@@ -348,8 +357,9 @@ void FuiPositionData::onPosTypeChanged(int option)
       continue;
 
 #ifdef FUI_DEBUG
-    std::cout <<"FuiPositionData::onPosTypeChanged: Location for "
-              << obj->getIdString() << loc << std::endl;
+    std::cout <<"FuiPositionData::onPosTypeChanged: ";
+    if (IAmEditingLinkCG) std::cout <<"CoG ";
+    std::cout <<"Location for "<< obj->getIdString() << loc << std::endl;
 #endif
 
     if (!loc.changePosType(PosTypeMapping::map()[option].first))
@@ -394,8 +404,9 @@ void FuiPositionData::onRotTypeChanged(int option)
       continue;
 
 #ifdef FUI_DEBUG
-    std::cout <<"FuiPositionData::onRotTypeChanged: Location for "
-              << obj->getIdString() << loc << std::endl;
+    std::cout <<"FuiPositionData::onRotTypeChanged: ";
+    if (IAmEditingLinkCG) std::cout <<"CoG ";
+    std::cout <<"Location for "<< obj->getIdString() << loc << std::endl;
 #endif
 
     if (!loc.changeRotType(RotTypeMapping::map()[option].first))
@@ -442,8 +453,9 @@ void FuiPositionData::FieldChangedCB::onFieldAccepted(double value)
       continue;
 
 #ifdef FUI_DEBUG
-    std::cout <<"FuiPositionData::onFieldAccepted: Location for "
-              << obj->getIdString() << loc << std::endl;
+    std::cout <<"FuiPositionData::onFieldAccepted: ";
+    if (owner->IAmEditingLinkCG) std::cout <<"CoG ";
+    std::cout <<"Location for "<< obj->getIdString() << loc << std::endl;
 #endif
 
     if (loc[i][j] == value)
@@ -624,8 +636,9 @@ void FuiPositionData::onPoppedUp()
     return;
 
 #ifdef FUI_DEBUG
-  std::cout <<"FuiPositionData::onPoppedUp: Location for "
-            << myEditedObj->getIdString() << loc << std::endl;
+  std::cout <<"FuiPositionData::onPoppedUp: ";
+  if (IAmEditingLinkCG) std::cout <<"CoG ";
+  std::cout <<"Location for "<< myEditedObj->getIdString() << loc << std::endl;
 #endif
   FaMat34 posCS = posRef ? posRef->getGlobalCS() : FaMat34();
   FaMat34 rotCS = rotRef ? rotRef->getGlobalCS() : FaMat34();