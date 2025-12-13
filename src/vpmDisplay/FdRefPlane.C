aa3392909893f6e02b25ecab9e6c9fa37c39b0e5
Knut Morten Okstad <kmo@openfedem.org>
2025-12-13 08:16:25 +0100

    Loop over the three material pointers to reduce code duplication

--- src/vpmDisplay/FdRefPlane.C
+++ src/vpmDisplay/FdRefPlane.C
@@ -34,20 +34,17 @@
 
 Fmd_SOURCE_INIT(FDREFPLANE,FdRefPlane,FdObject);
 
-FdRefPlane::FdRefPlane(FmRefPlane * pt) : FdObject()
+
+FdRefPlane::FdRefPlane(FmRefPlane* pt) : FdObject()
 {
   Fmd_CONSTRUCTOR_INIT(FdRefPlane);
 
   itsFmOwner = pt;
-  FdBackPointer * bp_pointer;
 
   itsKit = new FdRefPlaneKit;
   itsKit->ref();
 
-  //set up backpointer
-  
-  bp_pointer = SO_GET_PART(itsKit,"backPt",FdBackPointer);
-  bp_pointer->setPointer(this);
+  SO_GET_PART(itsKit,"backPt",FdBackPointer)->setPointer(this);
 
   this->highlightBoxId = NULL;
 }
@@ -67,48 +64,33 @@ SoNodeKitListPart* FdRefPlane::getListSw() const
 
 
 bool FdRefPlane::updateFdTopology(bool)
-{ 
+{
   return this->updateFdCS();
 }
 
 
 bool FdRefPlane::updateFdApperance()
 {
-  FmRefPlane * refPlane = (FmRefPlane*)itsFmOwner;
-   
-  //Plane Appearance:
+  FmRefPlane* refPlane = static_cast<FmRefPlane*>(itsFmOwner);
+  SbVec3f refpColor(FdConverter::toSbVec3f(refPlane->getRGBColor()));
 
-  SoMaterial* material = SO_GET_PART(itsKit,"planeMaterial",SoMaterial);
+  std::array<SoMaterial*,3> mats = {
+    SO_GET_PART(itsKit, "planeMaterial", SoMaterial),
+    SO_GET_PART(itsKit, "lineMaterial", SoMaterial),
+    SO_GET_PART(itsKit, "csAppearance.material", SoMaterial)
+  };
+  for (SoMaterial* material : mats) {
+    material->diffuseColor.setValue(refpColor);
+    material->ambientColor.setValue(refpColor);
+    material->specularColor.setValue(refpColor);
+    material->emissiveColor.setValue(SbVec3f(0.0f,0.0f,0.0f));
+    material->shininess = 0.9;
+  }
+  mats.front()->transparency.setValue(refPlane->getTransparency());
 
-  material->diffuseColor.setValue(FdConverter::toSbVec3f(refPlane->getRGBColor()));
-  material->ambientColor.setValue(FdConverter::toSbVec3f(refPlane->getRGBColor()));
-  material->emissiveColor.setValue(SbVec3f(0,0,0));
-  material->transparency.setValue((float)refPlane->getTransparency());
- 
-  // Frame Appearance:
+  SO_GET_PART(itsKit,"frameStyle",SoDrawStyle)->lineWidth.setValue(0);
+  SO_GET_PART(itsKit,"csStyle",SoDrawStyle)->lineWidth.setValue(0);
 
-  material = SO_GET_PART(itsKit,"lineMaterial",SoMaterial);
-
-  material->diffuseColor.setValue(FdConverter::toSbVec3f(refPlane->getRGBColor()));
-  material->ambientColor.setValue(FdConverter::toSbVec3f(refPlane->getRGBColor()));
-  material->specularColor.setValue(FdConverter::toSbVec3f(refPlane->getRGBColor()));
-  material->emissiveColor.setValue(SbVec3f(0,0,0));
-
-  SoDrawStyle *drawStyle = SO_GET_PART(itsKit,"frameStyle",SoDrawStyle);   
-  drawStyle->lineWidth.setValue(0);
-
-  // CsSymbol Appearance :
-
-  material = SO_GET_PART(itsKit,"csAppearance.material",SoMaterial);
-
-  material->diffuseColor.setValue(FdConverter::toSbVec3f(refPlane->getRGBColor()));
-  material->ambientColor.setValue(FdConverter::toSbVec3f(refPlane->getRGBColor()));
-  material->specularColor.setValue(FdConverter::toSbVec3f(refPlane->getRGBColor()));
-  material->emissiveColor.setValue(SbVec3f(0,0,0));
-
-  drawStyle = SO_GET_PART(itsKit,"csStyle",SoDrawStyle);   
-  drawStyle->lineWidth.setValue(0);
- 
   return true;
 }
 
@@ -116,46 +98,36 @@ bool FdRefPlane::updateFdApperance()
 bool FdRefPlane::updateFdCS()
 {
   SoTransform* transform = SO_GET_PART(itsKit,"planeTransform",SoTransform);
-  transform->setMatrix(FdConverter::toSbMatrix(((FmRefPlane*)itsFmOwner)->getLocalCS())); 
+  transform->setMatrix(FdConverter::toSbMatrix(static_cast<FmRefPlane*>(itsFmOwner)->getLocalCS()));
   return true;
-}  
+}
 
 
 bool FdRefPlane::updateFdDetails()
-{ 
+{
   // Make the refplane:
 
-  float x,y;
-  
-  x = (float)((FmRefPlane*)itsFmOwner)->getWidth();
-  y = (float)((FmRefPlane*)itsFmOwner)->getHeight();
-  
-  //SoDrawStyle * ds = SO_GET_PART(itsKit,"drawStyle", SoDrawStyle);
-
-  //if(((FmRefPlane*)itsFmOwner)->isVisible())
-  // ds->style.setValue(SoDrawStyle::LINES); 
-  // else 
-  //   ds->style.setValue(SoDrawStyle::INVISIBLE); 
+  float x = static_cast<FmRefPlane*>(itsFmOwner)->getWidth();
+  float y = static_cast<FmRefPlane*>(itsFmOwner)->getHeight();
 
   // Setting plane size and shape:
 
-  SoScale * planeScale =  SO_GET_PART(itsKit,"planeSize", SoScale);  
-  planeScale->scaleFactor.setValue(x,y,1);
+  SO_GET_PART(itsKit,"planeSize",SoScale)->scaleFactor.setValue(x,y,1);
 
-  SoCoordinate3 * coords =  SO_GET_PART(itsKit,"planeCoords",SoCoordinate3);  
+  SoCoordinate3* coords = SO_GET_PART(itsKit,"planeCoords",SoCoordinate3);
   coords->point.set1Value(0,-0.5,-0.5,0);
   coords->point.set1Value(1,-0.5,0.5,0);
-  coords->point.set1Value(2,0.5,0.5,0);			  
+  coords->point.set1Value(2,0.5,0.5,0);
   coords->point.set1Value(3,0.5,-0.5,0);
 
-  SoIndexedFaceSet * plane = SO_GET_PART(itsKit,"planeShape",SoIndexedFaceSet);
+  SoIndexedFaceSet* plane = SO_GET_PART(itsKit,"planeShape",SoIndexedFaceSet);
   plane->coordIndex.set1Value(0,0);
   plane->coordIndex.set1Value(1,1);
   plane->coordIndex.set1Value(2,2);
   plane->coordIndex.set1Value(3,3);
   plane->coordIndex.set1Value(4,SO_END_FACE_INDEX);
 
-  SoIndexedLineSet * frame = SO_GET_PART(itsKit,"frameShape",SoIndexedLineSet);
+  SoIndexedLineSet* frame = SO_GET_PART(itsKit,"frameShape",SoIndexedLineSet);
   frame->coordIndex.set1Value(0,0);
   frame->coordIndex.set1Value(1,1);
   frame->coordIndex.set1Value(2,2);
@@ -164,14 +136,12 @@ bool FdRefPlane::updateFdDetails()
   frame->coordIndex.set1Value(5,SO_END_FACE_INDEX);
 
   // Setting cs arrow symbol:
-  
-  SoTranslation * csTrans = SO_GET_PART(itsKit, "csTranslation",SoTranslation);
-  csTrans->translation.setValue(-0.45f*x,-0.45f*y,0);
+
+  SO_GET_PART(itsKit,"csTranslation",SoTranslation)->translation.setValue(-0.45f*x,-0.45f*y,0);
 
   itsKit->setPart("csSymbol",FdSymbolDefs::getSymbol(FdSymbolDefs::REFCS));
   itsKit->setPart("csSymbol.scale",NULL);
-  SoTransform* csScale = SO_GET_PART(itsKit,"csSymbol.scale", SoTransform);  
-  csScale->scaleFactor.setValue(0.1f*x,0.1f*y,1);
+  SO_GET_PART(itsKit,"csSymbol.scale",SoTransform)->scaleFactor.setValue(0.1f*x,0.1f*y,1);
 
   return true;
 }
@@ -179,33 +149,34 @@ bool FdRefPlane::updateFdDetails()
 
 void FdRefPlane::showHighlight()
 {
-  if(!FdDB::isUsingLineHighlight())
-    {
-      if (this->highlightBoxId)
-        {
-          FdExtraGraphics::removeBBox(this->highlightBoxId);
-          this->highlightBoxId = NULL;
-        }
-      this->highlightBoxId = FdExtraGraphics::showBBox(this->itsKit);
-    }
+  if (!FdDB::isUsingLineHighlight())
+  {
+    if (this->highlightBoxId)
+      FdExtraGraphics::removeBBox(this->highlightBoxId);
+    this->highlightBoxId = FdExtraGraphics::showBBox(this->itsKit);
+  }
   else
-    {
-      SoMaterial *lineMaterial   = SO_GET_PART(this->itsKit, "lineMaterial", SoMaterial);
-      SoMaterial *planeMaterial  = SO_GET_PART(this->itsKit, "planeMaterial", SoMaterial);
-      SoMaterial *symbolMaterial = SO_GET_PART(this->itsKit, "csAppearance.material", SoMaterial);
-      
-      FdSymbolDefs::makeMaterialHighlight(lineMaterial);
-      FdSymbolDefs::makeMaterialHighlight(planeMaterial);
-      FdSymbolDefs::makeMaterialHighlight(symbolMaterial);
+  {
+    std::array<SoMaterial*,3> mats = {
+      SO_GET_PART(itsKit, "planeMaterial", SoMaterial),
+      SO_GET_PART(itsKit, "lineMaterial", SoMaterial),
+      SO_GET_PART(itsKit, "csAppearance.material", SoMaterial)
+    };
+    // Set red highlight color
+    for (SoMaterial* mat : mats) {
+      mat->diffuseColor.setValue(1.0f,0.0f,0.0f);
+      mat->ambientColor.setValue(1.0f,0.0f,0.0f);
+      mat->emissiveColor.setValue(1.0f,0.0f,0.0f);
     }
+  }
 }
+
 
 void FdRefPlane::hideHighlight()
 {
-  if(this->highlightBoxId){
-    FdExtraGraphics::removeBBox(this->highlightBoxId);    
-    this->highlightBoxId = NULL;
-  }
-  
+  if (this->highlightBoxId)
+    FdExtraGraphics::removeBBox(this->highlightBoxId);
+
+  this->highlightBoxId = NULL;
   this->updateFdApperance();
 }
